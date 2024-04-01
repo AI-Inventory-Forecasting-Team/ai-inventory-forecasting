4e44b6e67fb66e4efc303ddc070f325210bc16c2
JooHyung <jhp21c@naver.com>
2024-04-01 23:04:09 +0900

    ♻️ 템플릿 파일과 URL 수정

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base_index.html'), name='index'),
     path('admin/', admin.site.urls),
+    path('base_contact.html', TemplateView.as_view(template_name='base_contact.html'), name='contact'),
+    path('base_about.html', TemplateView.as_view(template_name='base_about.html'), name='about'),
+    path('base_pricing.html', TemplateView.as_view(template_name='base_pricing.html'), name='pricing'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
