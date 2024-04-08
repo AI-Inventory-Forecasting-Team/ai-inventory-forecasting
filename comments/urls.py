1a98375fb8f0d2abacf0d76280cee2236136d73a
alsrb6013 <alsrb0217@gmail.com>
2024-04-08 17:54:18 +0900

    대댓글 추가(미완)

--- comments/urls.py
+++ comments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import CommentListView, CommentCreateView, CommentDetailView
+from .views import CommentListView, CommentCreateView, CommentDetailView, ReCommentCreateView, ReCommentListView, ReCommentDetailView
 
 urlpatterns = [
     path('', CommentListView.as_view(), name='comment-list-for-post'),
     path('create/', CommentCreateView.as_view(), name='comment-create'),
     path('<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
+    path('<int:comment_id>/replies/', ReCommentListView.as_view(), name='recomment-list-for-comment'),
+    path('<int:comment_id>/replies/create/', ReCommentCreateView.as_view(), name='recomment-create'),
+    path('replies/<int:pk>/', ReCommentDetailView.as_view(), name='recomment-detail'),
 ]
