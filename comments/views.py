1cce495f12d26875e0c16d7db281f1d04297ad2f
alsrb6013 <alsrb0217@gmail.com>
2024-04-08 17:24:00 +0900

    CRUD 코드 수정(미완)

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 from .models import Comment
 from .serializers import CommentSerializer
-from rest_framework import permissions
 
 class CommentCreateView(generics.CreateAPIView):
     queryset = Comment.objects.all()
@@ -11,16 +12,31 @@ class CommentCreateView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
 class CommentListView(generics.ListAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def get_queryset(self):
+        # Filters comments by post_id passed in the URL
         return self.queryset.filter(post_id=self.kwargs['post_id'])
 
-      
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Filters comments to those authored by the requesting user
+        return Comment.objects.filter(author=self.request.user)
+
+    def perform_update(self, serializer):
+        # Checks if the user is the author of the comment before updating
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this comment.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        # Checks if the user is the author of the comment before deleting
+        if instance.author != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this comment.")
+        instance.delete()
