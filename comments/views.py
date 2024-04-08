473d5b6d2d86876b916e37476f7ba169d8f40455
alsrb6013 <alsrb0217@gmail.com>
2024-04-08 17:54:18 +0900

    대댓글 추가(미완)

--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,8 @@
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import generics, permissions
 from rest_framework.exceptions import PermissionDenied
-from .models import Comment
-from .serializers import CommentSerializer
+from .models import Comment, ReComment
+from .serializers import CommentSerializer, ReCommentSerializer
+
 
 class CommentCreateView(generics.CreateAPIView):
     queryset = Comment.objects.all()
@@ -12,31 +12,45 @@ class CommentCreateView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class CommentListView(generics.ListAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def get_queryset(self):
-        # Filters comments by post_id passed in the URL
         return self.queryset.filter(post_id=self.kwargs['post_id'])
 
+
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Filters comments to those authored by the requesting user
         return Comment.objects.filter(author=self.request.user)
 
-    def perform_update(self, serializer):
-        # Checks if the user is the author of the comment before updating
-        comment = self.get_object()
-        if comment.author != self.request.user:
-            raise PermissionDenied("You do not have permission to edit this comment.")
-        serializer.save()
 
-    def perform_destroy(self, instance):
-        # Checks if the user is the author of the comment before deleting
-        if instance.author != self.request.user:
-            raise PermissionDenied("You do not have permission to delete this comment.")
-        instance.delete()
+class ReCommentCreateView(generics.CreateAPIView):
+    queryset = ReComment.objects.all()
+    serializer_class = ReCommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class ReCommentListView(generics.ListAPIView):
+    queryset = ReComment.objects.all()
+    serializer_class = ReCommentSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(comment_id=self.kwargs['comment_id'])
+
+
+class ReCommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ReComment.objects.all()
+    serializer_class = ReCommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return ReComment.objects.filter(author=self.request.user)
